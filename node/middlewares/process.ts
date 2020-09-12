d19625c83d6210810b598591b9bc91ec2eeb70b2
Dandy Hirama <danilo.hirama@vtex.com.br>
2020-09-12 09:38:55 -0300

    Add some template for request body parsing

--- node/middlewares/process.ts
+++ node/middlewares/process.ts
@@ -1,5 +1,15 @@
+import { json } from 'co-body'
+
 export async function process(ctx: Context, next: () => Promise<any>) {
   ctx.set('Cache-Control', 'no-cache')
+  
+  // Parse request body
+  const body = await json(ctx.req)
+  
+  // Perform any content calculation
+  console.log('Received body:', body)
+
+  // Define response
   ctx.response.status = 200      
   ctx.response.body = {
     sessionappnode:{
